e27ff65b242e5e5136f147c6db65b66b1d9138f5
Federico Gomez Beret <federicogomezberet@gmail.com>
2023-01-23 03:22:01 -0300

    form

--- formulario.js
+++ formulario.js
@@ -1,19 +1,32 @@
-const formulario = document.getElementById("formulario");
+const form = document.getElementById("my-form");
+const mail = document.getElementById("mail");
+form.onsubmit = (handleSubmit);
 
-formulario.onsubmit = (administarSubmit);
-
-async function administrarSubmit (evento) {
-evento.preventDefault()
-const form = new FormInfo (this)
-const response = await fetch (this.action, {
-    method: this.method,
-    body: form,
-    header: {
-        'Accept': 'application/json'
-    }
-})
-if (response.ok) {
-    this.reset ()
-    alert ("Formulario Enviado")
+async function handleSubmit(event) {
+event.preventDefault()
+const data = new FormData(this);
+const response = await fetch(this.action, {
+  method: this.method,
+  body: data,
+  headers: {
+    'Accept': 'application/json'
 }
-};
+})
+  if (response.ok) {
+    Swal.fire({
+        title: 'Formulario Enviado!',
+        text: 'Muchas gracias por contactarte con Huellitas®',
+        imageUrl: '../img/feliz.png',
+        imageAlt: 'Custom image',
+      })
+    form.reset()
+  } 
+ else {
+    Swal.fire({
+        title: 'Error!',
+        text: 'Existieron problemas al enviar el formulario',
+        imageUrl: '../img/nervioso.png',
+        imageAlt: 'Custom image',
+      })
+  }
+}
